48d174c4422feced589bacc77f2c9fb9d79b63b3
Akshai R Sangeeth <149455586+reformedbrush@users.noreply.github.com>
2026-04-12 23:30:14 +1000

    Auto skip test 1 netflix

---
+++ content/content.js
@@ -0,0 +1,12 @@
+console.log("Skipr running...");
+
+function skipIntro() {
+  const btn = document.querySelector('button[data-uia="player-skip-intro"]');
+
+  if (btn) {
+    console.log("Skipping intro...");
+    btn.click();
+  }
+}
+
+setInterval(skipIntro, 2000);
