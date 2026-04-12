95e22cdd3745000ee2bb96df9265259f9da60b35
Akshai R Sangeeth <149455586+reformedbrush@users.noreply.github.com>
2026-04-13 00:21:05 +1000

    Working feature/nextEpisode

--- content/content.js
+++ content/content.js
@@ -1,4 +1,5 @@
-console.log("Skipr running...");
+let timeout;
+console.log("Skipr running... fetaure/nextEp");
 
 function skipIntro() {
   const btn = document.querySelector('button[data-uia="player-skip-intro"]');
@@ -6,7 +7,33 @@ function skipIntro() {
   if (btn) {
     console.log("Skipping intro...");
     btn.click();
+    btn.dataset.clicked = "true";
   }
 }
 
-setInterval(skipIntro, 1500);
+function nextEpisode(){
+    const btn = document.querySelector('button[data-uia="next-episode-seamless-button"]');
+    if (btn){
+        console.log("Playing next episode...");
+        btn.click();
+        btn.dataset.clicked = "true";
+    }
+}
+
+function runSkipr(){
+    skipIntro();
+    nextEpisode();
+}
+
+const observer = new MutationObserver(()=>{
+    clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    runSkipr();
+  }, 300); 
+});
+
+observer.observe(document.body,{
+    childList: true,
+    subtree:true
+});
+
